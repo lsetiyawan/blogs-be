3185e4ab1eace93ea7d394afecdb2bb9a49957ad
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-31 18:55:10 +0700

    fix posts

--- src/article/article.controller.js
+++ src/article/article.controller.js
@@ -2,13 +2,24 @@ const { Article } = require("../database/models");
 
 class ArticleController {
   getAllArticles = async (req, res) => {
-    return await Article.findAll();
+    const allArticles = await Article.findAll();
+    return res.status(200).json(allArticles);
   };
 
   getSingleArticle = async (req, res) => {
     const { id } = req.params;
-    return await Article.findByPk(id);
+    try {
+      const singleArticle = await Article.findByPk(id);
+      if (singleArticle) {
+        return res.status(200).json(singleArticle);
+      }
+      return res.status(404).json({ message: "Article was not found" });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Error when fetching single article" });
+    }
   };
 }
 
-module.exports = new ArticleController;
+module.exports = new ArticleController();
